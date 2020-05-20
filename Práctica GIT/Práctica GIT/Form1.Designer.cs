85a1d3233a209ba3659b613d9400b83b595a9127
Aranza <aranzaiortegas@gmail.com>
2020-05-20 19:00:30 +0200

    Cambio telefono a movil

--- Práctica GIT/Práctica GIT/Form1.Designer.cs
+++ Práctica GIT/Práctica GIT/Form1.Designer.cs
@@ -90,11 +90,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(522, 48);
+            this.label2.Location = new System.Drawing.Point(539, 45);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.Size = new System.Drawing.Size(32, 13);
             this.label2.TabIndex = 8;
-            this.label2.Text = "Teléfono";
+            this.label2.Text = "Móvil";
             // 
             // textBox1
             // 
